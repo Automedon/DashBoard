1433aa052fb5b498bbc035aaf3cbad163a1b2640
Automedon <42299107+Automedon@users.noreply.github.com>
2020-03-09 19:29:23 +0700

    docs
    docs

---
+++ docs/precache-manifest.7df2cde00fd9138dd902a6fd14e4386c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "679312aa0ba75ee48823c6e5e096a5dd",
+    "url": "/DashBoard/index.html"
+  },
+  {
+    "revision": "65cb343b43cfcd153934",
+    "url": "/DashBoard/static/css/2.563af9af.chunk.css"
+  },
+  {
+    "revision": "f6e2c85bd8716955ad4e",
+    "url": "/DashBoard/static/css/main.5c145b84.chunk.css"
+  },
+  {
+    "revision": "65cb343b43cfcd153934",
+    "url": "/DashBoard/static/js/2.94cb6733.chunk.js"
+  },
+  {
+    "revision": "5a733b4f72d20e4afb54e249ef308592",
+    "url": "/DashBoard/static/js/2.94cb6733.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f6e2c85bd8716955ad4e",
+    "url": "/DashBoard/static/js/main.5557a727.chunk.js"
+  },
+  {
+    "revision": "1727a2b123835bec35f7",
+    "url": "/DashBoard/static/js/runtime-main.af087e3e.js"
+  }
+]);
